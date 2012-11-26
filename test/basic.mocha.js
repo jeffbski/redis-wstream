55d2086581e44ba7833b06031016eb870ae9f5cb
Jeff Barczewski <jeff.barczewski@gmail.com>
2012-11-26 08:06:28 -0600

    add binary data test

--- test/basic.mocha.js
+++ test/basic.mocha.js
@@ -2,6 +2,7 @@
 'use strict';
 
 var chai = require('chai-stack');
+var crypto = require('crypto');
 var redisWStream = require('..'); // require('redis-wstream');
 var passStream = require('pass-stream');
 var redis = require('redis');
@@ -59,7 +60,39 @@ test('basic use with Buffer, stream data is stored and end is fired', function (
   });
 });
 
+test('basic use with binary data in Buffers', function (done) {
+  var CHUNK_SIZE = 64 * 1024; // 64KB
+  var DATA_LENGTH = 2 * 1024 * 1024 + 25; // 2,025 KB
+  var shasum = crypto.createHash('sha1');
+  var resultDigest;
+  var bytesToGenerate = DATA_LENGTH;
+  var stream = redisWStream(client, KEY);
+  stream
+    .on('error', function (err) { done(err); })
+    .on('end', function () {
+      client.get(new Buffer(KEY), function (err, data) { // use Buffer key so returns Buffer data
+        if (err) return done(err);
+        var dataDigest = crypto.createHash('sha1').update(data).digest('base64');
+        t.equal(resultDigest, dataDigest);
+        done();
+      });
+    });
 
+  function gen() {
+    var size = (bytesToGenerate > CHUNK_SIZE) ? CHUNK_SIZE : bytesToGenerate;
+    var buff = crypto.randomBytes(size);
+    shasum.update(buff);
+    stream.write(buff);
+    bytesToGenerate -= size;
+    if (!bytesToGenerate) {
+      stream.end();
+      resultDigest = shasum.digest('base64');
+      return;
+    }
+    process.nextTick(gen); // use next tick so doesnt blow stack
+  }
+  process.nextTick(gen); // kick it off
+});
 
 
 test('all arguments missing for factory, throws error', function () {
